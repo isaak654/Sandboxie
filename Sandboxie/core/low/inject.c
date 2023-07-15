b091ce8c917438d23e969d51898794fd7a738c7e
isaak654 <isaak654@users.noreply.github.com>
2023-07-15 17:57:54 +0200

    typo test

--- Sandboxie/core/low/inject.c
+++ Sandboxie/core/low/inject.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2004-2020 Sandboxie Holdings, LLC 
  * Copyright 2020-2022 David Xanatos, xanasoft.com
- *
+ *  
  * This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
